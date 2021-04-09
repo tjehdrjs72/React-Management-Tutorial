3d1421d73fb0df78b28fec4cb7b7f8e49c7473bc
tjehdrjs72 <tjehdrjs72@naver.com>
2021-04-09 16:12:24 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 import './App.css'
 import Customer from './components/Customer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles =them =>({
+  root:{
+    width:'100%',
+    marginTop:them.spacing.unit *3,
+    overflowX:"auto"
+    },
+    table:{
+      minWidth:1080
+    }  
+})
+
 
 const customer=[{
   'id':1,
@@ -33,29 +52,27 @@ const customer=[{
 
 class App extends Component {
   render() {
+    const {classes}=this.props;
     return (
-      <div>
-        {
-          customer.map(c =>{
-            return(
-              <Customer
-              key={c.id}
-              id={c.id}
-              image={c.image}
-              name={c.name} 
-              birthday={c.birthday}
-              gender={c.gender}
-              job={c.job}
-              
-              />
-            )
-          })
-        }
-     
-     
-     </div>
+      <Paper className={classes.root}>
+        <Table className={classes.talbe}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+          { customer.map(c =>{return(<Customer key={c.id} id={c.id}  image={c.image} name={c.name}  birthday={c.birthday} gender={c.gender} job={c.job} />)}) }
+        </TableBody>
+        </Table>
+   </Paper>
     );
   }
 }
-export default App;
+export default withStyles(styles)(App);
 
